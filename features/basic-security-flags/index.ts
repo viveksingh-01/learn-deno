b3626ca6ff03c9dbe1e5f6d1dc3d0d49b33716b5
viveksingh-01 <viveksingh9301@gmail.com>
2020-09-04 14:26:58 +0530

    Demonstrate allow-write flag

--- features/basic-security-flags/index.ts
+++ features/basic-security-flags/index.ts
@@ -12,5 +12,20 @@ console.log(cwd);
 /**
  * @example Read content from a text-file
  */
-const fileContent = await Deno.readTextFile("./some-text-file.txt");
+const fileContent = await Deno.readTextFile("./read.txt");
 console.log(fileContent);
+
+/* ******************************************************************************************* */
+
+/**
+ * @name --allow-write
+ * @description Provides WRITE permissions to Deno
+ */
+
+/**
+ * @example Write content into file
+ */
+await Deno.writeTextFile(
+  "write.txt",
+  "This is some more random text...",
+);
